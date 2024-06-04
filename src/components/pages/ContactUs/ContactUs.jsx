2d7faeeb43fec009aa76aa2362dc19fae55f62ed
Mercy <durumercy206@gmail.com>
2024-06-04 10:23:38 +0100

    minor update

--- src/components/pages/ContactUs/ContactUs.jsx
+++ src/components/pages/ContactUs/ContactUs.jsx
@@ -9,7 +9,7 @@ const ContactUs = () => {
         style={{ backgroundImage: `url(${contact})` }}
         className=" bg-cover bg-center w-full h-screen md:h-[85vh] relative"
       >
-        <div className="absolute flex flex-col justify-center mx-7 md:mx-10 items-start  h-screen max-w-[70%] md:max-w-[50%] text-white">
+        <div className="absolute flex flex-col justify-center mx-7 md:mx-10 items-start  h-[550px] max-w-[70%] md:max-w-[50%] text-white">
           <h5 className="text-4xl md:text-5xl pb-4 font-bold">
             GET IN TOUCH WITH RCCG ROYAL EMBASSY{" "}
           </h5>
